13c1100481aad4364fdf5222dcd1bdf503e4838d
saviourgidi <profsaviour@gmail.com>
2017-05-11 17:56:40 +0000

    desktop notification and sample icon

--- public/js/app.js
+++ public/js/app.js
@@ -6,28 +6,47 @@ var textField = document.querySelector('input[name=text]');
 var button = document.querySelector('input[type=button]');
 var msg = document.querySelector('.response');
 
+
+
 /**
  * Transforms the form inputs
  */
 function send() {
-  var number = numberField.value.replace(/\D/g,''); // Remove all non-numeric chars
-  var text = textField.value;
-  
-  console.log("sending the inputs ");
- // fetch api to send the data 
-  fetch('/', {
-  method: 'post',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({number: number, text: text})
-})
-.then(function(res){ console.log(res) })
-.catch(function(error){ console.log(error)});
+    var number = numberField.value.replace(/\D/g, ''); // Remove all non-numeric chars
+    var text = textField.value;
+
+    console.log("sending the inputs ");
+    // fetch api to send the data 
+    fetch('/', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ number: number, text: text })
+        })
+        .then(function(res) { console.log(res) })
+        .catch(function(error) { console.log(error) });
 }
 
+
 textField.addEventListener('keyup', function(e) {
-  if ((e.keyCode || e.charCode) === 13) send();
+    if ((e.keyCode || e.charCode) === 13) send();
 }, false); // when a user presses a Return key
 
 button.addEventListener('click', send, false); // when a user click the "Send" button
+
+var socket = io();
+socket.on('smsStatus', function(data) {
+    displayStatus('Message ID ' + data.id + ' successfully sent to ' + data.number);
+});
+
+Notification.requestPermission().then(function(status) {
+    console.log(status);
+});
+
+function displayStatus(message) {
+    var notification = new Notification('nexmo', {
+        body: message,
+        icon: 'images/icon.png'
+    })
+}
